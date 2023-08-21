7986798c4dcdc0afc3d7347ce7697f9afe4173d2
SowingHustle <hazewry00@gmail.com>
2023-08-21 11:28:07 -0300

    altered the longitude and altitude

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
   language: "en", // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: "37.775",
-  defaultLongitude: "-122.419",
+  defaultLatitude: "-22.133750",
+  defaultLongitude: "-51.425180",
 
   // Autochange
   autoChangeTheme: true,
